c48269cd04e365b6de861ae049e1d77ce9b06e47
Michele Montalvo <michelemontalvo@outlook.com>
2025-04-30 11:41:15 +0200

    LAB-temporary_tables_views_CTE
    lab completed

---
+++ MySQL-temporary_tables_views_CTE.sql
@@ -0,0 +1,29 @@
+CREATE OR REPLACE VIEW customer_rental_summary AS
+SELECT 
+    c.customer_id,
+    CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
+    c.email,
+    COUNT(r.rental_id) AS rental_count
+FROM customer c
+JOIN rental r ON c.customer_id = r.customer_id
+GROUP BY c.customer_id, customer_name, c.email;
+
+CREATE TEMPORARY TABLE customer_payment_summary AS
+SELECT 
+    crs.customer_id,
+    SUM(p.amount) AS total_paid
+FROM customer_rental_summary crs
+JOIN payment p ON crs.customer_id = p.customer_id
+GROUP BY crs.customer_id;
+
+WITH customer_summary AS (
+    SELECT 
+        crs.customer_name,
+        crs.email,
+        crs.rental_count,
+        cps.total_paid,
+        ROUND(cps.total_paid / crs.rental_count, 2) AS average_payment_per_rental
+    FROM customer_rental_summary crs
+    JOIN customer_payment_summary cps ON crs.customer_id = cps.customer_id
+)
+SELECT * FROM customer_summary;
